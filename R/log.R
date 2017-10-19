a674d920fd1c9b736de7500dfb6e25ff2969a672
Garrett Dancik <garrett.dancik@gmail.com>
2017-10-18 22:48:09 -0400

    Remove files from master branch

--- R/log.R
+++
@@ -1,7 +0,0 @@
-saveLog <- function(e)UseMethod("saveLog")
-
-saveLog.default <- function(e){
-  # save log
-  suppressMessages(suppressWarnings(
-    saveRDS(e$log, file.path(e$udat, paste0(as.integer(Sys.time()), ".swlog")))))
-}
